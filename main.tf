25c172eef70d8cd23d79dab10abd9673788e83ef
kiran7028 <kiran7028@gmail.com>
2025-12-10 18:46:53 +0530

    added yml file 3

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "cicd-ec2" {
   ami           = "ami-0d176f79571d18a8f"
   instance_type = "t3.micro"
   tags = {
-    Name = "CICD-ec2-manualapproval"
+    Name = "CICD-ec2-manualappr"
   }
 }
