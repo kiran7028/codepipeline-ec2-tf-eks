d5c40750fc191243fc75fecfac1e58f319b799ab
kiran7028 <kiran7028@gmail.com>
2025-12-11 01:05:15 +0530

    added dev and prod tf files

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "environment" {
   type        = string
   description = "Environment name (dev/prod)"
-  default     = "dev"
+  #default     = "dev"
   #required    = true
   #sensitive   = false
   #visibility  = "sensitive"
-  validation {
-    condition     = contains(["dev", "prod"], var.environment)
-    error_message = "Environment must be 'dev' or 'prod'."
-  }
+  #validation {
+   # condition     = contains(["dev", "prod"], var.environment)
+    #error_message = "Environment must be 'dev' or 'prod'."
+  #}
 }
 
 variable "instance_type" {
