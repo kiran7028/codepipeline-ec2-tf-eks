19dc63c6959f2802728495d7163786c7a4bedde1
kiran7028 <kiran7028@gmail.com>
2025-12-10 19:07:18 +0530

    added apply and remove cloudwatch

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "cicd-ec2" {
   ami           = "ami-0d176f79571d18a8f"
   instance_type = "t3.micro"
   tags = {
-    Name = "CICD-ec2-added apply and remove cloudwatch"
+    Name = "CICD-ec2-Instance"
   }
 }
